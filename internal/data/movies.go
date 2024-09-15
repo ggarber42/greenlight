8b47c9cce636631475b930e956ce4ce550c61677
ggarber42 <ggarber@protonmail.com>
2024-09-15 15:11:55 -0300

    remove CreatedAt from json response

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,7 +6,7 @@ import (
 
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year"`
 	Runtime   int32     `json:"run_time"`

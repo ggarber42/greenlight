5535f6a172e56b89c7aaa18b53c6ac950e6ef160
ggarber42 <ggarber@protonmail.com>
2024-09-15 19:46:37 -0300

    create a validation rule for input fields

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/ggarber42/greenlight/internal/data"
+	"github.com/ggarber42/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +21,24 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 	} else {
-		fmt.Fprintf(w, "%+v\n", input)
+
+
+		movie := &data.Movie{
+			Title: input.Title,
+			Year: input.Year,
+			Runtime: input.Runtime,
+			Genres: input.Genres,
+		}
+
+		v := validator.New()
+
+		if data.ValidateMovie(v, movie); !v.Valid(){
+			app.failedValidationResponse(w, r, v.Errors)
+		} else {
+			fmt.Fprintf(w, "%+v\n", input)
+		}
+
+
 	}
 
 }

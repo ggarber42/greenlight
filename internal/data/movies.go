ab9edc2985c413b3f2545486f25fc63acc0854ad
ggarber42 <ggarber@protonmail.com>
2024-09-15 15:52:18 -0300

    add custom json encoding to runtime

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -9,7 +9,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"run_time,omitempty"`
+	Runtime   Runtime     `json:"run_time,omitempty,string"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }

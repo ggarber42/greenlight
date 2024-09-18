ab1a2b64a0f9c44b68bf553039c60c2f3e3e6193
ggarber42 <ggarber@protonmail.com>
2024-09-17 23:11:51 -0300

    add todo mock

--- internal/data/models.go
+++ internal/data/models.go
@@ -11,6 +11,12 @@ var (
 
 type Models struct {
 	Movies MovieModel
+	// Movies interface { Todo
+	// 	Insert(movie *Movie) error
+	// 	Get(id int64) (*Movie, error)
+	// 	Update(movie *Movie) error
+	// 	Delete(id int64) error
+	// 	}
 }
 
 func NewModels(db *sql.DB) Models {
@@ -18,3 +24,9 @@ func NewModels(db *sql.DB) Models {
 		Movies: MovieModel{DB: db},
 	}
 }
+
+// func NewMockModels() Models { Todo
+// 	return Models{
+// 		Movies: MockMovieModel{},
+// 	}
+// }

5f6bb147889af75ccd116cdbe0bf3d6bfb79be7c
ggarber42 <ggarber@protonmail.com>
2024-09-18 21:26:33 -0300

    insert mode

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -11,10 +11,10 @@ import (
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime data.Runtime    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
@@ -22,22 +22,30 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 	} else {
 
-
 		movie := &data.Movie{
-			Title: input.Title,
-			Year: input.Year,
+			Title:   input.Title,
+			Year:    input.Year,
 			Runtime: input.Runtime,
-			Genres: input.Genres,
+			Genres:  input.Genres,
 		}
 
 		v := validator.New()
 
-		if data.ValidateMovie(v, movie); !v.Valid(){
+		if data.ValidateMovie(v, movie); !v.Valid() {
 			app.failedValidationResponse(w, r, v.Errors)
 		} else {
-			fmt.Fprintf(w, "%+v\n", input)
+			err := app.models.Movies.Insert(movie)
+			if err != nil {
+				app.serverErrorResponse(w, r, err)
+			} else {
+				headers := make(http.Header)
+				headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+				err := app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+				if err != nil {
+					app.serverErrorResponse(w, r, err)
+				}
+			}
 		}
-
 
 	}
 
